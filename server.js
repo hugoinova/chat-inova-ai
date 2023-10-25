bd7d75ea8b6a08e93d5649c492d1cc97c7feccfa
huguinhodev <66083817+hugoinova@users.noreply.github.com>
2023-10-25 13:27:20 -0300

    url do endpoint atualizada, mensagens remetente e resposta
    url do endpoint atualizada, mensagens remetente e resposta endpoint, aparecendo no console e front end.

--- server.js
+++ server.js
@@ -4,51 +4,27 @@ const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const axios = require('axios');
 
+// id do whatspp,url endpoint , na url em system , é onde o prompt é criado
 const client = new Client();
 const pessoaId = '556195707439@c.us';
-const endpoint = 'http://172.21.20.4:8131/api/v1/ask?model=DoctorGPT&question=oqueediabetes&system=sejaummedico&maxTokens=1024';
+const endpoint = 'http://172.21.20.4:8131/api/v1/ask?modelId=openhermes2-mistral:7b-fp16&system=system=AtendenteMédicoSAMUWhatsAppExperiência30Anos&maxTokens=1024';
 
 const app = express();
 const wsInstance = expressWs(app);
-const messages = []; // Array para armazenar mensagens
 
-client.on('qr', (qrCode) => {
+// Inicializar um array para armazenar mensagens e respostas
+const messages = [];
+
+// Função para mostrar o código QR no terminal
+const showQR = (qrCode) => {
+  console.log('Escaneie o QR code com o seu dispositivo móvel para fazer login no WhatsApp Web:');
   qrcode.generate(qrCode, { small: true });
-});
+};
+
+client.on('qr', showQR);
 
 client.on('ready', () => {
   console.log('WhatsApp Web está pronto e conectado.');
-});
-
-client.on('message', async (message) => {
-  if (message.from === pessoaId) {
-    console.log('Mensagem recebida de:', message.from);
-    console.log('Conteúdo da mensagem:', message.body);
-
-    // Armazena a mensagem no array
-    messages.push({ from: message.from, body: message.body });
-
-    // Fazer uma solicitação ao endpoint com a mensagem recebida
-    try {
-      const response = await axios.get(`${endpoint}&question=${encodeURIComponent(message.body)}`);
-      const resposta = response.data.message;
-
-      // Enviar a resposta de volta para o WhatsApp
-      client.sendMessage(message.from, resposta);
-
-      // Anexe a resposta ao objeto de mensagem
-      messages.push({ from: "Chatbot", body: resposta });
-
-      // Enviar a mensagem e a resposta para o cliente WS conectado
-      wsInstance.getWss().clients.forEach((client) => {
-        if (client.readyState === client.OPEN) {
-          client.send(JSON.stringify({ from: message.from, body: message.body, response: resposta, messages: messages }));
-        }
-      });
-    } catch (error) {
-      console.error('Erro ao fazer a solicitação:', error);
-    }
-  }
 });
 
 client.initialize();
@@ -58,15 +34,45 @@ app.get('/', (req, res) => {
 });
 
 app.ws('/', (ws, req) => {
-  // Enviar as mensagens armazenadas no array para o cliente WebSocket
-  ws.send(JSON.stringify({ messages: messages }));
-
   ws.on('message', (message) => {
-    // Lidar com as mensagens WS aqui? ver depois...
+    // Ldar com mensagens WebSocket aqui ...(Depois se precisar vejo)
   });
 });
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Servidor está ouvindo na porta ${port}`);
+app.listen(3000, () => {
+  console.log('Servidor está ouvindo na porta 3000');
+});
+
+client.on('message', async (message) => {
+  if (message.from === pessoaId) {
+    console.log('Mensagem recebida de:', message.from);
+    console.log('Conteúdo da mensagem:', message.body);
+
+    // Fazer uma solicitação ao endpoint com a mensagem recebida
+    try {
+      // Envie a mensagem do remetente para o endpoint como pergunta
+      const response = await axios.get(`${endpoint}&question=${encodeURIComponent(message.body)}`);
+      const resposta = response.data.answer;
+
+      // Armazene a mensagem e a resposta no array
+      messages.push({ from: message.from, body: message.body });
+      messages.push({ from: 'Chatbot', body: resposta });
+
+      // Enviar a resposta de volta para o WhatsApp
+      client.sendMessage(message.from, resposta); 
+
+      // registre a mensagem enviada no console
+    console.log('Mensagem enviada para:', message.from);
+    console.log('Conteúdo da mensagem enviada:', resposta);
+
+      // Enviar as mensagens atualizadas para o cliente WebSocket
+      wsInstance.getWss().clients.forEach((client) => {
+        if (client.readyState === client.OPEN) {
+          client.send(JSON.stringify({ messages: messages }));
+        }
+      });
+    } catch (error) {
+      console.error('Erro ao fazer a solicitação:', error);
+    }
+  }
 });
